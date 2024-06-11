db94c766241376f5b8c80123bd81efb80974bf51
Son Le <leminhson2398@outlook.com>
2024-06-12 05:44:09 +0700

    Update game.py

--- game.py
+++ game.py
@@ -1,10 +1,9 @@
 import sys
-import pygame
-import square
 import utils
 import pieces
 import board
 from game_types import GameInterface
+from typing import Optional, Any
 
 SAMPLE = """
 		Black
@@ -25,15 +24,20 @@ class Game(GameInterface):
     """Game holds logic of chess game"""
 
     def __init__(self):
-        self.__board: dict[str, pieces.Piece] = {}
+        self.__board: dict[str, Optional[pieces.Piece]] = {}
         self.__screen = None
 
         self.last_file = utils.File_A
         self.last_rank = utils.Rank_1
-        self.__available_moves: set[str] = {}
+        self.__available_moves: set[str] = set()
 
         # indicate which side can move
         self.__turn = utils.Color.BLACK
+        self.__active_square: Optional[str] = None
+        self.__captures_data = {
+            utils.Color.BLACK: [],
+            utils.Color.WHITE: [],
+        }
 
     def set_screen(self, screen: board.GameRenderer):
         """set display screen for the game"""
@@ -41,10 +45,9 @@ class Game(GameInterface):
 
     def switch_turn(self) -> None:
         """Toggle turns for players"""
-        if self.__turn == utils.Color.WHITE:
-            self.__turn = utils.Color.BLACK
-            return
-        self.__turn = utils.Color.WHITE
+        self.__turn = (
+            utils.Color.BLACK if self.__turn == utils.Color.WHITE else utils.Color.WHITE
+        )
 
     def __init_board(self) -> None:
         for rank in utils.RANKS:
@@ -73,7 +76,9 @@ class Game(GameInterface):
                 square_name = utils.create_square_name(file, item[1])
 
                 self.__board[square_name] = piece
-                self.__screen.draw_on_square(square_name, piece=piece)
+                self.__screen.draw_piece_on_square(
+                    square_name, piece_name=piece.__str__()
+                )
 
             piece_class = piece_classes[index]
             for item in [
@@ -84,9 +89,11 @@ class Game(GameInterface):
                 square_name = utils.create_square_name(file, item[1])
 
                 self.__board[square_name] = piece
-                self.__screen.draw_on_square(square_name, piece=piece)
+                self.__screen.draw_piece_on_square(
+                    square_name, piece_name=piece.__str__()
+                )
 
-    def get_board(self) -> dict[str, square.Square]:
+    def get_board(self) -> dict[str, Optional[Any]]:
         """getter for accessing game board state"""
         return self.__board
 
@@ -118,34 +125,65 @@ class Game(GameInterface):
         If yes, returns `True`, `False` otherwise.
         If `square_name` is outside of board, return `True`
         """
-        if not square_name in self.__board:
-            return True
-        return self.__board[square_name] is not None
+        return square_name not in self.__board or self.__board[square_name] is not None
 
-    def handle_mouse_down(self, event: pygame.event.Event):
-        """listener for left mouse click up event"""
-        (position_x, position_y) = event.dict["pos"]
-        file = position_x // utils.SQUARE_SIZE + utils.File_A
-        rank = position_y // utils.SQUARE_SIZE + utils.Rank_1
+    def move_piece_from_source_to_dest(self, source_sq: str, dest_sq: str) -> None:
+        if source_sq not in self.__board or dest_sq not in self.__board:
+            return
+        self.__screen.draw_piece_on_square(dest_sq, self.__board[source_sq].__str__())
+        original_color = utils.SQUARES_COLOR_MAP[source_sq]
+        self.__screen.set_color_on_square(source_sq, original_color)
+        self.__board[dest_sq] = self.__board[source_sq]
+        self.__board[source_sq] = None
 
-        square_name = utils.create_square_name(file, rank)
-        piece = self.__board.get(square_name)
-        if not piece and square_name in self.__available_moves:
-            self.__screen.draw_on_square(square_name, piece=piece)
-            self.__screen.draw_on_square()
-            self.__available_moves.clear()
-        else:
-            self.__available_moves = piece.calculate_next_moves((file, rank), self)
-            for move in self.__available_moves:
-                self.__screen.draw_on_square(move, True)
+    def handle_cell_select(self, event: utils.GameEvent):
+        """listener for left mouse click event"""
 
-    def event_handler(self, event: pygame.event.Event):
+        file_rank = event.to_file_rank()
+        selected_square_name = utils.create_square_name(*file_rank)
+
+        if self.__active_square and len(self.__available_moves) > 0:
+            # means user is moving pieces
+            if selected_square_name in self.__available_moves:
+                for sq_name in self.__available_moves | {self.__active_square}:
+                    original_color = utils.SQUARES_COLOR_MAP[sq_name]
+                    self.__screen.set_color_on_square(sq_name, original_color)
+
+                self.move_piece_from_source_to_dest(
+                    self.__active_square, selected_square_name
+                )
+                self.__board
+                self.__active_square = None
+                self.__available_moves = set()
+                self.switch_turn()
+
+            else:
+                for cell_name in self.__available_moves:
+                    original_color = utils.SQUARES_COLOR_MAP[cell_name]
+                    self.__screen.set_color_on_square(cell_name, original_color)
+                self.__active_square = None
+                self.__available_moves = set()
+
+        elif self.check_selected_cell_is_valid_turn(selected_square_name):
+            # means user is choosing piece to move
+            piece = self.__board[selected_square_name]
+            self.__available_moves = piece.calculate_next_moves(file_rank, self)
+            self.__active_square = selected_square_name
+
+            for square_name in self.__available_moves:
+                self.__screen.set_color_on_square(square_name, utils.Color.GREEN)
+
+    def event_handler(self, event: utils.GameEvent):
         """event listener for game events"""
-        if event.type == pygame.QUIT:
+        if event.event_type == utils.GameEventType.QUIT:
             self.__close()
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                self.handle_mouse_down(event)
+        elif event.event_type == utils.GameEventType.MOUSE_CLICK:
+            self.handle_cell_select(event)
+
+    def check_selected_cell_is_valid_turn(self, square_name: str) -> bool:
+        """check if selected cell contains a piece with color match turn color"""
+        piece = self.__board.get(square_name)
+        return piece and piece.color == self.__turn
 
     def run(self):
         """run the game"""
