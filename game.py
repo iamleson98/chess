ff63033175ce1dca9b5d965c6447fc3bc8db73cb
Son Le <leminhson2398@outlook.com>
2024-06-15 10:27:37 +0700

    Update game.py

--- game.py
+++ game.py
@@ -26,12 +26,16 @@ class Game(GameInterface):
     def __init__(self):
         self.__board: dict[str, Optional[pieces.Piece]] = {}
         self.__screen = None
+
         self.last_file = utils.File_A
         self.last_rank = utils.Rank_1
         self.__available_moves: set[str] = set()
-        self.__turn_color = utils.Color.BLACK
+
+        # indicate which side can move
+        self.__turn = utils.Color.BLACK
+        self.__winner: utils.Color = None
         self.__active_square: Optional[str] = None
-        self.__captures_data = {
+        self.__captures_data: dict[utils.Color, list[pieces.Piece]] = {
             utils.Color.BLACK: [],
             utils.Color.WHITE: [],
         }
@@ -42,8 +46,8 @@ class Game(GameInterface):
 
     def switch_turn(self) -> None:
         """Toggle turns for players"""
-        self.__turn_color = (
-            utils.Color.BLACK if self.__turn_color == utils.Color.WHITE else utils.Color.WHITE
+        self.__turn = (
+            utils.Color.BLACK if self.__turn == utils.Color.WHITE else utils.Color.WHITE
         )
 
     def __init_board(self) -> None:
@@ -106,7 +110,7 @@ class Game(GameInterface):
         piece_1 = self.__board.get(square_name_1)
         piece_2 = self.__board.get(square_name_2)
 
-        return piece_1 and piece_2 and piece_2.color != piece_1.color
+        return piece_1 and piece_2 and piece_1.color != piece_2.color
 
     def check_square_occupied(self, square_name: str) -> bool:
         """
@@ -120,59 +124,83 @@ class Game(GameInterface):
         if source_sq not in self.__board or dest_sq not in self.__board:
             return
 
-        self.__screen.draw_piece_on_square(dest_sq, self.__board[source_sq].__str__())
-        original_color = utils.SQUARES_COLOR_MAP[source_sq]
-        self.__screen.set_color_on_square(source_sq, original_color)
-        self.__board[dest_sq] = self.__board[source_sq]
+        source_piece = self.__board.get(source_sq)
+        captured_piece = self.__board.get(dest_sq)
+
+        if captured_piece:
+            self.__captures_data[self.__turn].append(captured_piece)
+            if captured_piece.piece_type == utils.PieceType.KING:
+                self.__winner = self.__turn
+                self.__close()
+
+        source_sq_original_color = utils.SQUARES_COLOR_MAP[source_sq]
+        dest_sq_original_color = utils.SQUARES_COLOR_MAP[dest_sq]
+        self.__screen.set_color_on_square(source_sq, source_sq_original_color)
+        self.__screen.set_color_on_square(dest_sq, dest_sq_original_color)
+        self.__screen.draw_piece_on_square(dest_sq, source_piece.__str__())
+
+        self.__board[dest_sq] = source_piece
         self.__board[source_sq] = None
 
-    def handle_cell_select(self, event: utils.GameEvent):
+    def unset_color_for_available_moves(self) -> None:
+        for sq_name in self.__available_moves:
+            original_color = utils.SQUARES_COLOR_MAP[sq_name]
+            self.__screen.set_color_on_square(
+                sq_name,
+                original_color,
+            )
+
+            if piece := self.__board.get(sq_name):
+                self.__screen.draw_piece_on_square(sq_name, piece.__str__())
+
+    def handle_square_select(self, event: utils.GameEvent):
         """listener for left mouse click event"""
 
         file_rank = event.to_file_rank()
         selected_square_name = utils.create_square_name(*file_rank)
 
         if self.__active_square and len(self.__available_moves) > 0:
-            # means user is moving pieces
-            if selected_square_name in self.__available_moves:
-                for sq_name in self.__available_moves | {self.__active_square}:
-                    original_color = utils.SQUARES_COLOR_MAP[sq_name]
-                    self.__screen.set_color_on_square(sq_name, original_color)
+            # means user is moving a piece
+            self.unset_color_for_available_moves()
 
+            if selected_square_name in self.__available_moves:
+                # means user have choosen valid move
                 self.move_piece_from_source_to_dest(
                     self.__active_square, selected_square_name
                 )
                 self.__active_square = None
-                self.__available_moves = set()
                 self.switch_turn()
-
             else:
-                for cell_name in self.__available_moves:
-                    original_color = utils.SQUARES_COLOR_MAP[cell_name]
-                    self.__screen.set_color_on_square(cell_name, original_color)
+                # means user discards move
                 self.__active_square = None
-                self.__available_moves = set()
 
-        elif self.check_selected_square_holds_piece_in_turn(selected_square_name):
+            self.__available_moves = set()
+
+        elif self.check_selected_square_is_valid_turn(selected_square_name):
             # means user is choosing piece to move
             piece = self.__board[selected_square_name]
-            self.__available_moves = piece.calculate_next_moves(file_rank, self)
+            self.__available_moves = piece.calculate_available_moves(file_rank, self)
             self.__active_square = selected_square_name
 
             for square_name in self.__available_moves:
                 self.__screen.set_color_on_square(square_name, utils.Color.GREEN)
+                piece_on_square = self.__board.get(square_name)
+                if piece_on_square:
+                    self.__screen.draw_piece_on_square(
+                        square_name, piece_on_square.__str__()
+                    )
 
     def event_handler(self, event: utils.GameEvent):
         """event listener for game events"""
         if event.event_type == utils.GameEventType.QUIT:
             self.__close()
         elif event.event_type == utils.GameEventType.MOUSE_CLICK:
-            self.handle_cell_select(event)
+            self.handle_square_select(event)
 
-    def check_selected_square_holds_piece_in_turn(self, square_name: str) -> bool:
+    def check_selected_square_is_valid_turn(self, square_name: str) -> bool:
         """check if selected cell contains a piece with color match turn color"""
         piece = self.__board.get(square_name)
-        return piece and piece.color == self.__turn_color
+        return piece and piece.color == self.__turn
 
     def run(self):
         """run the game"""
